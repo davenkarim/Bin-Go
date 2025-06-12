87c566d6b533cafe1393de3ded2d908d711981f4
davenkarim <davenchen988@gmail.com>
2025-06-12 10:21:48 +0700

    First Commit

---
+++ BinGo/BinGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinGoApp.swift
+//  BinGo
+//
+//  Created by Daven Karim on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BinGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
